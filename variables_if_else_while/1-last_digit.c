159316eb046563844300d3a945faa8c90acb6e0f
Gülanə Mehrəlizadə <gulanemehrelizade97@gmail.com>
2024-02-23 17:11:47 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,13 +14,21 @@ int main(void)
 	srand(time(0));
 	n = rand();
 
-	printf("Last digit of %d is %d", n, n % 10);
-	if (n % 10 > 5)
-		printf(" and is greater than 5\n");
-	else if (n % 10 == 0)
-		printf(" and is 0\n");
+	printf("Last digit of %d is ", n);
+
+	if (n < 0)
+		n *= -1; /* Make n positive if it's negative */
+
+	n %= 10; /* Extract the last digit */
+
+	printf("%d ", n);
+
+	if (n > 5)
+		printf("and is greater than 5\n");
+	else if (n == 0)
+		printf("and is 0\n");
 	else
-		printf(" and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 
 	return (0);
 }
