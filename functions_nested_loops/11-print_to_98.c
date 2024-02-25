77900fd03169900a936f1d90328525179a475d14
Gülanə Mehrəlizadə <gulanemehrelizade97@gmail.com>
2024-02-25 19:37:08 +0000

    print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ *
+ * Description: This function prints all natural numbers from the given
+ * starting number 'n' up to 98, inclusive, separated by a comma and space.
+ * If 'n' is greater than 98, it prints the numbers in descending order.
+ * If 'n' is 98, it prints only 98.
+ */
 void print_to_98(int n)
 {
 
