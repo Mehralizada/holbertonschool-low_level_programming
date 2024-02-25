90a5171c96bdb2945dfa898a060a9b3c9b73a0ce
Gülanə Mehrəlizadə <gulanemehrelizade97@gmail.com>
2024-02-25 19:10:16 +0000

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 
-void print_to_98(int n) {
-    int i;
+void print_to_98(int n)
+{
 
-    if (n <= 98) {
-        for (i = n; i < 98; i++) {
-            printf("%d, ", i);
-        }
-    } else {
-        for (i = n; i > 98; i--) {
-            printf("%d, ", i);
-        }
-    }
-    printf("98\n");
+
+int i;
+
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{
+printf("%d, ", i);
 }
-
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+}
+}
+printf("98\n");
+}
