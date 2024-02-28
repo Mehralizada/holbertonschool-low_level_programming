acd81c62f990e59c241edef2db110e14ff5c2a4d
root <Gulane Mehrelizade>
2024-02-28 14:56:30 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > )
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; i < size; i++)
+			{
+				for (j = 0; j < size; j++)
+				{
+					_putchar('#');
+				}
+				_putchar('\n');
+			}
+		}
+		else
+		{
+			_putchar('\n');
+		}
