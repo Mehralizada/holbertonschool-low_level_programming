4e9896e1071131383d413e262d8e503948586b87
Gulane Mehrelizade <gulanemehrelizade97@gmail.com>
2024-03-03 16:48:01 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- * Return: Always 0.
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ * Return: If str1 < str2, the negative.
+ * If str1 == str2, 0.
+ * If str1 > str2, the positive difference of the first unmatched characters.
  */
-int main(void)
-{
-	char s1[] = "Hello";
-	char s2[] = "World!";
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
