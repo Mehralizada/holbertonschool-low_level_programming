a24acb53f0ab23c5b6172e78faefcf6f5136ffc9
root <Gulane Mehrelizade>
2024-03-03 06:23:49 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,35 +7,33 @@
  */
 int _atoi(char *s)
 {
-    int i = 0, n = 0, len = 0, digit = 0, sign = 1;
+int i = 0, n = 0, len = 0, digit = 0, sign = 1;
 
 
-    while (s[len] != '\0')
-        len++;
+while (s[len] != '\0')
+len++;
 
 
-    while (i < len)
-    {
+while (i < len)
+{
 
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            digit = s[i] - '0';
-
-
-            n = n * 10 + digit;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
 
 
-            if (s[i + 1] < '0' || s[i + 1] > '9')
-                break;
-        }
+n = n * 10 + digit;
 
-        else if (s[i] == '-' && (i == 0 || s[i - 1] < '0' || s[i - 1] > '9'))
-        {
-            sign *= -1;
-        }
-        i++;
-    }
-
-    return n * sign;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
 }
 
+else if (s[i] == '-' && (i == 0 || s[i - 1] < '0' || s[i - 1] > '9'))
+{
+sign *= -1;
+}
+i++;
+}
+
+return (n *sign);
+}
