9277f6fca299ed4ee30f4f207832e7f90aad9a3d
root <Gulane Mehrelizade>
2024-03-03 05:21:29 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,41 +3,39 @@
 /**
  * _atoi - Convert a string to an integer.
  * @s: The pointer to convert
- * Return: A integer
+ * Return: An integer
  */
-
 int _atoi(char *s)
-
 {
-	int i, d, n, len, f, digit;
+    int i = 0, n = 0, len = 0, digit = 0, sign = 1;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
 
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '_')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-	return (n);
+    while (s[len] != '\0')
+        len++;
+
+
+    while (i < len)
+    {
+
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            digit = s[i] - '0';
+
+
+            n = n * 10 + digit;
+
+
+            if (s[i + 1] < '0' || s[i + 1] > '9')
+                break;
+        }
+
+        else if (s[i] == '-' && (i == 0 || s[i - 1] < '0' || s[i - 1] > '9'))
+        {
+            sign *= -1;
+        }
+        i++;
+    }
+
+    return n * sign;
 }
+
