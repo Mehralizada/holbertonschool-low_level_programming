9f0d97aa4e282e3eda4c2cb38eb7374406edf085
Gulane Mehrelizade <gulanemehrelizade97@gmail.com>
2024-03-08 19:02:13 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,12 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-		putchar(*s);
-		s++;
-		_puts_recursion(s);
+	else
+		_putchar('\n');
 }
