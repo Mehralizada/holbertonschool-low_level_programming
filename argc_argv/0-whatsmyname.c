6bd0a8d0adefe5d62825d0f581fcdff8e029c645
Gulane Mehrelizade <gulanemehrelizade97@gmail.com>
2024-03-09 08:14:31 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always zero
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", *argv);
+	return (0);
+}
