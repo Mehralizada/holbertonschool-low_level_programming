421920633f0f458115a9a5d0b82e82ebcfcbdb6d
Gulane Mehrelizade <gulanemehrelizade97@gmail.com>
2024-03-09 16:23:43 +0000

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,20 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1 = 0, n2 = 0;
-
-	if (argc == 3)
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+        (void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 
 
