ced1a89de68123520023e6c6697bb0912537cd25
Gulane Mehrelizade <gulanemehrelizade97@gmail.com>
2024-03-10 15:20:22 +0000

    100-change.c

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,13 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 #include <unistd.h>
+#include "main.h"
 
 /* Function prototypes */
 int _putchar(char c);
 void _puts(char *str);
 void _putnbr(int n);
 int main(int argc, char *argv[]);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
 
